ceefdb06c8403eb27ee1007703ba259f8e104b2d
Maarten Litmaath <Maarten.Litmaath@cern.ch>
2025-08-18 19:52:37 +0200

    Various improvements in the HTML conversion.

--- make-html.sh
+++ make-html.sh
@@ -2,15 +2,19 @@
 # Assumes "pandoc" has been installed (available from EPEL).
 # As its current HTML-to-PDF engine is unsupported since several years,
 # we let it convert just to HTML and do the PDF conversion elsewhere.
-# Replace binary unicode sequences: some PDF converters cannot handle them.
+# We first need to fix and adjust a few things in the HTML, though.
 
 out=profile.html
-keep='(compute\.\w+ *)+|(storage\.\w+:/\S* *)+|scope|wlcg\.groups|(/\w+)+'
+keep='https:\S+|(compute\.\w+ *)+|(storage\.\w+:/\S* *)+|scope|wlcg\.groups|(/\w+)+'
 
-pandoc -o $out -css profile.css profile.md &&
+pandoc --ascii -o $out -css profile.css profile.md &&
     perl -i -pe '
-	s/(href="#)[0-9]+-+/$1/;
-	s/^(<h[1-6] +id="[^"]*)\./$1/;
+	s/(href="#)(\d+-)/$1s$2/;
+	if (s/^(<h[1-6] +id=")([^"]+">)((\d+\.)+)/$1s$3-$2/) {
+	    while (s/^(<h[1-6] +id="[^"]*)\./$1/) {
+	    }
+	}
+	s,&#x201C\;('"$keep"')&#x201D\;,"$1",g;
 	s,\xE2\x80\x9C('"$keep"')\xE2\x80\x9D,"$1",g;
 	s/\xE2\x80\x9C/&ldquo\;/g;
 	s/\xE2\x80\x9D/&rdquo\;/g;
@@ -18,6 +22,7 @@ pandoc -o $out -css profile.css profile.md &&
 	s/\xE2\x80\x99/&rsquo\;/g;
 	s/\xE2\x80\x94/&mdash\;/g;
 	s/\xC2\xA0/&nbsp\;/g;
-	s/(<a [^>]+>)\xE2\x86\xA9\xEF\xB8\x8E/&nbsp;&nbsp;&nbsp;$1&#x21A9\;/g;
+	s/(<a [^>]+>)\xE2\x86\xA9\xEF\xB8\x8E/&nbsp\;&nbsp\;&nbsp\;$1&#x21A9\;/g;
+	s/(<a [^>]+>&#x21A9\;)/&nbsp\;&nbsp\;&nbsp\;$1/g;
     ' $out
 
